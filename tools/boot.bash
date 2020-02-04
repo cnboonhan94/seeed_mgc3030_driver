174065e89ef9f3b661048af062332ec946e26d57
Boon Han <cnboonhan94@gmail.com>
2020-02-04 22:57:40 +0800

    update

--- tools/boot.bash
+++ tools/boot.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 source /opt/ros/kinetic/setup.bash
 source $HOME/seeed_ws/devel/setup.bash
-roslaunch seeed_mgc3030_driver mgc3030_cmd_vel.launch
+roslaunch seeed_mgc3030_driver mgc3030_cmd_vel_turtle.launch
